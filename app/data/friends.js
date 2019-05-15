f4822068a179c5e4fb5ded5d9643f73b3ca3a40d
dvshannon96@gmail.com <dvshannon96@gmail.com>
2019-05-15 17:46:38 -0400

    added markup for html with api routes

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+// defining friends
+const friends = [{
+    name: 'Pierson',
+    photo: 'https://www.instagram.com/p/BsgoWwIFobe/?utm_source=ig_web_copy_link',
+    scores: [
+        '5',
+        '3',
+        '4',
+        '2',
+        "4",
+        "4",
+        "5",
+        '2'
+    ]
+    },
+    {
+    name: 'Chandler',
+    photo: 'https://www.instagram.com/p/BIYMOp0jt6H/?utm_source=ig_web_button_share_sheet',
+    scores: [
+        '3',
+        '5',
+        '3',
+        '4',
+        "3",
+        "4",
+        "5",
+        '2'
+    ]
+    }
+]
+
+// export friends to other files
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,11 @@
+const friends = require('..data/friends');
+
 module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
-        // res.json();
+        res.json(friends);
       });
     
       app.post("/api/friends", function(req, res) {
-        // res.json();
+        res.json();
       });
 };
